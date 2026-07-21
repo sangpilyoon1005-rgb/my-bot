37bd6a339260af7db0b87e91577e7f82ab98cbca
sangpilyoon1005-rgb <sangpilyoon1005@gmail.com>
2026-07-22 00:10:07 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,19 +10,6 @@ from discord import app_commands
 import aiohttp
 from aiohttp import web
 
-async def health_check(request):
-  return web.Response(text="OK", status=200)
-
-async def start_web_server():
-  app = web.Application()
-  app.router.add_get('/health', health_check) # Health Check API 추가
-  runner = web.AppRunner(app)
-  await runner.setup()
-  site = web.TCPSite(runner, '0.0.0.0', 8000)
-  await site.start()
-
-
-
 # =========================
 # 🔑 TOKEN
 # =========================
